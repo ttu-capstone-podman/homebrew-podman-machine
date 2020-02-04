0c09a2964a1113fd9876dea891a0ad49f2b37c8f
Aaron Otto <atotto42@students.tntech.edu>
2020-02-04 12:08:25 -0600

    Update podman-machine.rb

--- Formula/podman-machine.rb
+++ Formula/podman-machine.rb
@@ -1,1 +1,16 @@
-TODO: add formula code
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PodmanMachine < Formula
+  desc ""
+  homepage "https://github.com/boot2podman/machine"
+  url "https://github.com/boot2podman/machine/releases/download/v0.17/podman-machine.darwin-amd64"
+  sha256 "079bc9941d7479214149d0d38599489fab1164f56721b333714b8817cef5e88a"
+
+
+  def install
+    mv "podman-machine.darwin-amd64", "podman-machine"
+    bin.install "podman-machine"
+    
+  end
+end
