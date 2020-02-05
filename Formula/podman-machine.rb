1867f784d28e681c754038f7412cca0f839ef0f9
Aaron Otto <atotto42@students.tntech.edu>
2020-02-05 15:46:41 -0600

    Update podman-machine.rb

--- Formula/podman-machine.rb
+++ Formula/podman-machine.rb
@@ -6,7 +6,7 @@ class PodmanMachine < Formula
   homepage "https://github.com/boot2podman/machine"
   url "https://github.com/boot2podman/machine/releases/download/v0.17/podman-machine.darwin-amd64"
   sha256 "079bc9941d7479214149d0d38599489fab1164f56721b333714b8817cef5e88a"
-
+  depends_on "virtualbox" => :install
 
   def install
     mv "podman-machine.darwin-amd64", "podman-machine"
