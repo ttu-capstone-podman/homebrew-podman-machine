8a75169d659f832662c0054aab43c4b6b364e6c5
albornzineCahill <legojonah39@gmail.com>
2020-03-30 14:22:20 -0500

    added a script that is expected to fail for testing purposes

---
+++ Formula/podman-machine-fail.rb
@@ -0,0 +1,18 @@
+class PodmanMachine < Formula
+  desc ""
+  homepage "https://github.com/boot2podman/machine"
+  url "https://github.com/boot2podman/machine/releases/download/v0.17/podman-machine.darwin-amd64"
+  sha256 "079bc9941d7479214149d0d38599489fab1164f56721b333714b8817cef5e88a"
+
+   depends_on 'qemu'
+
+  def install
+    mv "podman-machine.darwin-amd64", "podman-machine"
+    bin.install "podman-machine"
+    
+  end
+
+  test do
+    assert_equal "podman-machine version dev, build fb1f14e", shell_output("podman-machine --version")
+  end
+end
